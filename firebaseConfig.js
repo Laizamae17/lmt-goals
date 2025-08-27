4d784c65531a67061911349acebe7bb529f9eb1a
Laizamae17 <laizamaetomarong@gmail.com>
2025-08-27 09:47:01 +0800

    Firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXhg8eQ30K9FZaVRaUwRgx0tNEBz2yTY8",
+  authDomain: "lmt-goals.firebaseapp.com",
+  projectId: "lmt-goals",
+  storageBucket: "lmt-goals.firebasestorage.app",
+  messagingSenderId: "1050525470107",
+  appId: "1:1050525470107:web:05d1296aff470cf70c653e"
+};
+
+
+const app = initializeApp(firebaseConfig);
