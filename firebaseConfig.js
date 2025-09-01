2d78e90c277b5600d1f666e0cbc4809d76297707
Laizamae17 <laizamaetomarong@gmail.com>
2025-09-01 13:07:36 +0800

    Goals Context

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXhg8eQ30K9FZaVRaUwRgx0tNEBz2yTY8",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
